2c90bf311d9799cb995111c41d0d9b8efc59c5e8
imrahulkanna <kannarahul05@gmail.com>
2024-05-09 00:52:12 +0530

    adding isObject constraint before finding its length

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,7 +1,7 @@
 export function isDataEmptyorUndefined(data: any) {
     return (
         (Array.isArray(data) && data.length === 0) ||
-        Object.keys(data).length === 0 ||
+        typeof data === "object" && Object.keys(data).length === 0 ||
         data === undefined ||
         data === null ||
         data === "" ||
