e47b75c25ac1cd71e438802f5224366b9c631769
rahul kanna <kannarahul05@gmail.com>
2024-10-18 01:28:38 +0530

    updated the login modal

--- components/LoginModal.tsx
+++ components/LoginModal.tsx
@@ -1,24 +1,29 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Cross2Icon, EyeClosedIcon, EyeOpenIcon } from "@radix-ui/react-icons";
 import InputBox from "./InputBox";
 import Link from "next/link";
 
 interface props {
-    closeLoginModal: (
-        e: React.MouseEvent<HTMLButtonElement> | React.MouseEvent<HTMLDivElement>
-    ) => void;
+    closeLoginModal: (e: React.MouseEvent<HTMLButtonElement> | React.MouseEvent<HTMLDivElement>) => void;
 }
+
 const LoginModal = ({ closeLoginModal }: props) => {
     const [showPassword, setShowPassword] = useState(false);
-    const handleLogin = (
-        e: React.MouseEvent<HTMLButtonElement> | React.MouseEvent<HTMLDivElement>
-    ) => {
-        closeLoginModal(e);
-    };
+
+    useEffect(() => {
+        const body = document.querySelector("body");
+        body?.classList.add("overflow-hidden");
+        return () => {
+            body?.classList.remove("overflow-hidden");
+        };
+    }, []);
 
     const toggleShowPassword = () => {
         setShowPassword(!showPassword);
+    };
+
+    const handleSubmit = async () => {
     };
 
     return (
@@ -36,39 +41,51 @@ const LoginModal = ({ closeLoginModal }: props) => {
                         <Cross2Icon stroke="10" />
                     </button>
                     <h3 className="text-center font-semibold text-2xl py-5">Welcome back!</h3>
-                    <form className="flex flex-col gap-6 mb-6 items-end h-auto">
-                        <InputBox type="email" placeholder="Email" />
-                        <InputBox type={showPassword ? "text" : "password"} placeholder="Password">
-                            <div
-                                className="invisible peer-focus:visible hover:visible absolute right-2 top-1/2 -translate-y-1/2 z-[51] cursor-pointer"
-                                onClick={toggleShowPassword}
+                    <form action={handleSubmit}>
+                        <div className="flex flex-col gap-6 mb-6 items-end h-auto">
+                            <InputBox type="email" placeholder="Email" />
+                            <InputBox
+                                type={showPassword ? "text" : "password"}
+                                placeholder="Password"
                             >
-                                {showPassword ? <EyeOpenIcon /> : <EyeClosedIcon />}
-                            </div>
-                        </InputBox>
-                        <Link href="#" className="text-blue-400 text-sm -mt-4">
-                            Forgot password?
-                        </Link>
+                                <div
+                                    className="invisible peer-focus:visible hover:visible absolute right-2 top-1/2 -translate-y-1/2 z-[51] cursor-pointer"
+                                    onClick={toggleShowPassword}
+                                >
+                                    {showPassword ? <EyeOpenIcon /> : <EyeClosedIcon />}
+                                </div>
+                            </InputBox>
+                            <Link href="#" className="text-blue-400 text-sm -mt-4">
+                                Forgot password?
+                            </Link>
+                        </div>
+                        <button
+                            className="bg-primary w-full py-2 mb-4 rounded text-neutral-800 font-bold"
+                            type="submit"
+                        >
+                            Login
+                        </button>
+                        <button
+                            className="bg-neutral-100 w-full py-2 rounded text-neutral-800 font-bold"
+                            type="submit"
+                        >
+                            Register
+                        </button>
+                        <div className="w-full mt-6 mb-4 flex justify-center items-center gap-1">
+                            <div className="w-full h-px bg-neutral-500" />
+                            <p className="">or</p>
+                            <div className="w-full h-px bg-neutral-500" />
+                        </div>
+                        <button
+                            className="bg-neutral-100 w-full py-1 rounded text-neutral-800 font-bold"
+                            type="submit"
+                            name="action"
+                            value="google"
+                        >
+                            Continue with Google
+                        </button>
                     </form>
-                    <button
-                        className="bg-primary w-full py-3 mb-4 rounded text-neutral-800 font-bold"
-                        onClick={handleLogin}
-                    >
-                        Login
-                    </button>
-                    <button
-                        className="bg-neutral-100 w-full py-3 rounded text-neutral-800 font-bold"
-                        onClick={handleLogin}
-                    >
-                        Register
-                    </button>
                 </div>
-                <div className="w-full mt-6 mb-4 flex justify-center items-center gap-1">
-                    <div className="w-full h-px bg-neutral-500" />
-                    <p className="">or</p>
-                    <div className="w-full h-px bg-neutral-500" />
-                </div>
-                <div>login with google</div>
             </div>
         </div>
     );
