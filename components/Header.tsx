d80dbd402a532a2fff476bfddebb85a9c6463acc
rahul kanna <kannarahul05@gmail.com>
2024-10-19 17:00:56 +0530

    added profile pic(google)

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,10 +4,12 @@ import Image from "next/image";
 import { Button } from "./ui/button";
 import LoginModal from "./LoginModal";
 import { signOut, useSession } from "next-auth/react";
+import { isDataEmptyorUndefined } from "@/app/lib/utils";
+import Logo from "./Logo";
 
 const Header = ({ rowdies }: { rowdies: any }) => {
     const [openLoginModal, setOpenLoginModal] = useState<boolean>(false);
-    const {data: session, status} = useSession();
+    const { data: session, status } = useSession();
 
     const handleLoginModal = (
         e: React.MouseEvent<HTMLButtonElement> | React.MouseEvent<HTMLDivElement>
@@ -32,15 +34,22 @@ const Header = ({ rowdies }: { rowdies: any }) => {
     return (
         <>
             <header className="z-50 top-0 left-1/2 right-0 -translate-x-1/2 fixed w-full bg-neutral-800/80 backdrop-blur">
-                <div className="w-full max-w-[1800px] mx-auto px-4 md:px-10 3xl:px-2 flex justify-between items-center">
+                <div className="w-full max-w-[1800px] mx-auto px-4 md:px-10 3xl:px-2 py-4 flex justify-between items-center">
                     <div className="flex items-center gap-3">
-                        <Image src="/anidex-logo.png" alt="anidex logo" width={50} height={50} />
-                        <p className={`text-4xl font-extrabold text-primary ${rowdies.className}`}>
-                            AniDex
-                        </p>
+                        <Logo width={150} height={50} />
                     </div>
-                    {
-                        status === "authenticated" ? (
+                    {status === "authenticated" && !isDataEmptyorUndefined(session) ? (
+                        <div className="flex items-center gap-2">
+                            <p className="font-semibold">Hi, {session?.user?.name?.split(" ")[0] || "user"}</p>
+                            {session?.user?.image && (
+                                <Image
+                                    src={session.user.image as string}
+                                    alt="profile-image"
+                                    width={40}
+                                    height={40}
+                                    className="rounded-full"
+                                ></Image>
+                            )}
                             <Button
                                 variant="secondary"
                                 className="bg-neutral-100 font-semibold hover:bg-neutral-100/80 hover:scale-105"
@@ -48,16 +57,16 @@ const Header = ({ rowdies }: { rowdies: any }) => {
                             >
                                 Logout
                             </Button>
-                        ) : (
-                            <Button
-                                variant="secondary"
-                                className="bg-primary font-semibold hover:bg-primary/80 hover:scale-105"
-                                onClick={handleLoginModal}
-                            >
-                                Login
-                            </Button>
-                        )
-                    }
+                        </div>
+                    ) : (
+                        <Button
+                            variant="secondary"
+                            className="bg-primary font-semibold hover:bg-primary/80 hover:scale-105"
+                            onClick={handleLoginModal}
+                        >
+                            Login
+                        </Button>
+                    )}
                 </div>
             </header>
             {openLoginModal && <LoginModal closeLoginModal={handleLoginModal} />}
