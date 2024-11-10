69f53da2d5878a71352a0935f560ce1549580351
rahul kanna <kannarahul05@gmail.com>
2024-11-10 22:59:20 +0530

    created verify-email api

---
+++ app/api/verify-email/route.ts
@@ -0,0 +1,32 @@
+import dbConnect from "@/app/lib/dbConnect";
+import UserModel from "@/model/User";
+
+export async function POST(request: Request) {
+    try {
+        await dbConnect();
+        const { email, enteredCode } = await request.json();
+        const userDetails = await UserModel.findOne({ email });
+
+        if (userDetails && userDetails.verifyCode == enteredCode) {
+            userDetails.isVerified = true;
+            await userDetails.save();
+
+            return Response.json(
+                {
+                    success: true,
+                    message: "Email verified successfully",
+                },
+                { status: 200 }
+            );
+        } else {;
+            return Response.json(
+                {
+                    success: true,
+                    message: "Incorrect verification code. Try again",
+                },
+                { status: 200 }
+            );
+        }
+        return Response.json({});
+    } catch (error) {}
+}
