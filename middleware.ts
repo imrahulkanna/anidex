1f1e288fbe463515d713f1f79e501d44cc3b6a01
rahulkanna <kannarahul05@gmail.com>
2025-01-09 00:11:55 +0530

    reverting the protectedRoutes var and updating middleware

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,6 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-const protectedRoutes = ["/user/:path*"];
-
 export async function middleware(request: NextRequest) {
     const token = await getToken({ req: request });
 
@@ -12,5 +10,5 @@ export async function middleware(request: NextRequest) {
     }
 }
 export const config = {
-    matcher: protectedRoutes,
+    matcher: ["/user/:path*"],
 };
