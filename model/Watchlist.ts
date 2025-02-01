b7440c70d20d212dfe3f51b7a5e0a107f8778291
rahulkanna <kannarahul05@gmail.com>
2025-02-01 19:40:25 +0530

    updating the schema of watchlist

--- model/Watchlist.ts
+++ model/Watchlist.ts
@@ -2,45 +2,54 @@ import mongoose, { Schema, Document } from "mongoose";
 
 export interface Watchlist extends Document {
     userId: Schema.Types.ObjectId;
-    watching: number[];
-    onHold: number[];
-    planToWatch: number[];
-    dropped: number[];
-    completed: number[];
+    watchlist: {
+        watching: number[];
+        onHold: number[];
+        planToWatch: number[];
+        dropped: number[];
+        completed: number[];
+    };
+    createdAt: Date;
+    updatedAt: Date;
 }
-const WatchlistSchema: Schema = new Schema<Watchlist>({
-    userId: {
-        type: Schema.Types.ObjectId,
-        required: true,
-        ref: "User", // Reference to the User collection
-        unique: true,
+const WatchlistSchema: Schema = new Schema<Watchlist>(
+    {
+        userId: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: "User", // Reference to the User collection
+            unique: true,
+        },
+        watchlist: {
+            watching: {
+                type: [Schema.Types.Number],
+                default: [],
+                required: true,
+            },
+            onHold: {
+                type: [Schema.Types.Number],
+                default: [],
+                required: true,
+            },
+            planToWatch: {
+                type: [Schema.Types.Number],
+                default: [],
+                required: true,
+            },
+            dropped: {
+                type: [Schema.Types.Number],
+                default: [],
+                required: true,
+            },
+            completed: {
+                type: [Schema.Types.Number],
+                default: [],
+                required: true,
+            },
+        },
     },
-    watching: {
-        type: [Schema.Types.Number],
-        default: [],
-        required: true,
-    },
-    onHold: {
-        type: [Schema.Types.Number],
-        default: [],
-        required: true,
-    },
-    planToWatch: {
-        type: [Schema.Types.Number],
-        default: [],
-        required: true,
-    },
-    dropped: {
-        type: [Schema.Types.Number],
-        default: [],
-        required: true,
-    },
-    completed: {
-        type: [Schema.Types.Number],
-        default: [],
-        required: true,
-    },
-});
+    { timestamps: true }
+);
 
 const WatchlistModel =
     (mongoose.models.Watchlist as mongoose.Model<Watchlist>) ||
