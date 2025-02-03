b4c722d039e92a7b0949214e9b6186e37ad28669
rahulkanna <kannarahul05@gmail.com>
2025-02-03 18:08:21 +0530

    implemented favourites page

---
+++ app/user/favourites/page.tsx
@@ -0,0 +1,78 @@
+"use client";
+import { isDataEmptyorUndefined } from "@/app/lib/utils";
+import { useUserData } from "@/context/UserDataContext";
+import React, { useEffect, useState } from "react";
+import { WatchlistAnimesSkeletion } from "../watchlist/page";
+import { animeData } from "@/components/TrendingAnimeList";
+import HoverCardWrapper from "@/components/HoverCardWrapper";
+import Image from "next/image";
+import Link from "next/link";
+import { DotFilledIcon } from "@radix-ui/react-icons";
+
+const Favourites = () => {
+    const { userData } = useUserData();
+    const [favouriteAnimes, setFavouriteAnimes] = useState([]);
+
+    useEffect(() => {
+        if (!userData?._id) {
+            return;
+        }
+
+        const fetchFavouritAnimesData = async () => {
+            const response = await fetch(`/api/get-favourites-details/${userData._id}`, {
+                method: "GET",
+                cache: "no-cache",
+            });
+            const data = await response.json();
+
+            setFavouriteAnimes(data.data);
+        };
+
+        fetchFavouritAnimesData();
+    }, [userData]);
+
+    return (
+        <div className="mx-auto lg:w-3/4">
+            {isDataEmptyorUndefined(userData) || isDataEmptyorUndefined(favouriteAnimes) ? (
+                <WatchlistAnimesSkeletion />
+            ) : (
+                <div className="flex flex-wrap justify-center [&>*:nth-child(odd)]:px-2 [&>*:nth-child(even)]:px-2 md:[&>*:nth-child(odd)]:px-4 md:[&>*:nth-child(even)]:px-4">
+                    {favouriteAnimes.map((anime: animeData) => (
+                        <div key={anime.mal_id} className="w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-4">
+                            <div className="my-4 relative">
+                                <HoverCardWrapper anime={anime}>
+                                    <Image
+                                        src={anime.images.webp.large_image_url}
+                                        alt={anime.title_english || anime.title}
+                                        width={300}
+                                        height={0}
+                                        className="w-full h-[250px] md:h-[300px] xl:h-[250px] 2xl:h-[320px] object-cover"
+                                    />
+                                    <div className="absolute inset-0 bg-gradient-to-t from-obsidian to-30%" />
+                                </HoverCardWrapper>
+                            </div>
+                            <Link href="#">
+                                <p className="whitespace-nowrap overflow-hidden text-ellipsis font-bold hover:text-primary">
+                                    {anime.title_english || anime.title}
+                                </p>
+                            </Link>
+                            <div className="flex items-center gap-0.5 text-sm">
+                                <div className="opacity-60 font-medium">{anime.type || "TV"}</div>
+                                <DotFilledIcon className={`opacity-30`} />
+                                <div className="opacity-60">
+                                    {anime.episodes ? (
+                                        <span className={`font-medium`}>{anime.episodes} eps</span>
+                                    ) : (
+                                        <span>N/A</span>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Favourites;
--- app/user/watchlist/page.tsx
+++ app/user/watchlist/page.tsx
@@ -20,10 +20,10 @@ interface watchlistAnimeProps {
 export const WatchlistAnimesSkeletion = () => {
     return (
         <div className="flex flex-wrap justify-center [&>*:nth-child(odd)]:px-2 [&>*:nth-child(even)]:px-2 md:[&>*:nth-child(odd)]:px-4 md:[&>*:nth-child(even)]:px-4 mt-4">
-            {[...Array(10)].map(() => (
-                <div className="w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-4">
+            {[...Array(10)].map((id) => (
+                <div key={id} className="w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 mb-4">
                     <div className="w-full h-[250px] md:h-[300px] xl:h-[250px] 2xl:h-[320px] bg-gray-500 mb-2"></div>
-                    <p className="w-3/4 bg-gray-500 rounded h-2 whitespace-nowrap overflow-hidden text-ellipsis font-bold hover:text-primary mb-2"></p>
+                    <p className="w-3/4 bg-gray-500 rounded h-2 whitespace-nowrap overflow-hidden text-ellipsis font-bold mb-2"></p>
                     <div className="flex items-center gap-0.5 text-sm">
                         <div className="w-5 bg-gray-500 rounded h-2"></div>
                         <DotFilledIcon className={`opacity-30`} />
@@ -51,7 +51,7 @@ const DisplayAnime = ({ anime }: { anime: animeData }) => {
                 </HoverCardWrapper>
             </div>
             <Link href="#">
-                <p className="whitespace-nowrap overflow-hidden text-ellipsis font-bold hover:text-primary">
+                <p className="whitespace-nowrap overflow-hidden text-ellipsis font-bold">
                     {anime.title_english || anime.title}
                 </p>
             </Link>
@@ -143,10 +143,10 @@ const Watchlist = () => {
             <div className="w-full flex gap-x-2 md:gap-x-4 gap-y-2 justify-center flex-wrap px-2 md:px-4">
                 {Object.entries(options).map(([name, option]) => (
                     <div
-                        className={`px-6 py-2 cursor-pointer rounded-md hover:text-primary font-semibold text-xs md:text-sm ${
+                        className={`px-6 py-2 cursor-pointer rounded-md font-semibold text-xs md:text-sm ${
                             option.selected
-                                ? "bg-primary text-neutral-800 hover:text-neutral-800"
-                                : "bg-white/10"
+                                ? "bg-primary text-neutral-800"
+                                : "bg-white/10 hover:text-primary"
                         }`}
                         onClick={() => handleOptionClick(name as WatchlistOption)}
                     >
