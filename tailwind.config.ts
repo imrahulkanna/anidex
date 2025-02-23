1bc7e090e8ffff904de2a1846156f41489001033
rahulkanna <kannarahul05@gmail.com>
2025-02-23 14:44:48 +0530

    added an animation style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { transform } from "next/dist/build/swc";
 import type { Config } from "tailwindcss";
 
 const config = {
@@ -24,10 +25,15 @@ const config = {
                     from: { height: "var(--radix-accordion-content-height)" },
                     to: { height: "0" },
                 },
+                "loader-bounce": {
+                    "0%, 80%, 100%": { transform: "scale(0)" },
+                    "40%": { transform: "scale(1)" },
+                },
             },
             animation: {
                 "accordion-down": "accordion-down 0.2s ease-out",
                 "accordion-up": "accordion-up 0.2s ease-out",
+                "loader-bounce": "loader-bounce 1.4s infinite ease-in-out both",
             },
             screens: {
                 "2xl": "1670px",
