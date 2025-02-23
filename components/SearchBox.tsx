a9df5d7c22d295d4209d2e28f67e446af26d3419
rahulkanna <kannarahul05@gmail.com>
2025-02-23 14:44:33 +0530

    showing loader in the results

--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -6,41 +6,57 @@ import { animeData } from "@/types/ApiResponse";
 import { DotFilledIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
-const DisplaySearchAnimeResults = ({ data }: { data: animeData[] | [] }) => {
+interface SeachAnimePropTypes {
+    data: animeData[] | [];
+    showLoader: boolean;
+}
+
+const DisplaySearchAnimeResults = ({ data, showLoader }: SeachAnimePropTypes) => {
     return (
-        <ul className="absolute w-full top-[102%] bg-neutral-700 shadow-[5px_20px_20px_rgba(0,0,0,0.6)] text-xs text-white font-semibold rounded">
-            {isDataEmptyorUndefined(data) ? (
-                <li className="p-3">No results found</li>
-            ) : (
-                data.slice(0, 5).map((anime: animeData) => (
-                    <li
-                        key={anime.mal_id}
-                        className="w-full flex p-3 gap-2 border-b border-white/20 last:border-none hover:bg-white/10 group"
-                    >
-                        <Image
-                            src={anime.images.webp.large_image_url}
-                            alt={anime.title_english || anime.title}
-                            width={50}
-                            height={70}
-                            title={anime.title_english || anime.title}
-                            className="object-none rounded"
-                        />
-                        <div className="w-full flex flex-col justify-around">
-                            <p className="whitespace-nowrap overflow-hidden text-ellipsis text-sm w-[218px] group-hover:text-primary">
-                                {anime.title_english || anime.title}
-                            </p>
-                            <p>{anime.type}</p>
-                            <div className="flex gap-0.5 items-center">
-                                <p>{anime.aired.string.split(" to")[0]}</p>
-                                <DotFilledIcon className="opacity-30" />
-                                <p>{anime.duration}</p>
-                            </div>
-                        </div>
-                    </li>
-                ))
+        <div className="absolute w-full top-[102%] bg-neutral-700 shadow-[5px_20px_20px_rgba(0,0,0,0.6)] text-xs text-white font-semibold rounded">
+            {showLoader && (
+                <div className="w-full flex items-center justify-center p-3 gap-5">
+                    <span className="rounded-[100%] w-4 h-4 border bg-neutral-50 animate-loader-bounce "></span>
+                    <span className="rounded-[100%] w-4 h-4 border bg-neutral-50 animate-loader-bounce delay-100"></span>
+                    <span className="rounded-[100%] w-4 h-4 border bg-neutral-50 animate-loader-bounce delay-200"></span>
+                </div>
             )}
-        </ul>
+            <div>
+                {isDataEmptyorUndefined(data) ? (
+                    <div className="p-3">No results found</div>
+                ) : (
+                    data.slice(0, 5).map((anime: animeData) => (
+                        <Link
+                            href=""
+                            key={anime.mal_id}
+                            className="w-full flex p-3 gap-2 border-b border-white/20 last:border-none hover:bg-white/10 group"
+                        >
+                            <Image
+                                src={anime.images.webp.large_image_url}
+                                alt={anime.title_english || anime.title}
+                                width={50}
+                                height={70}
+                                title={anime.title_english || anime.title}
+                                className="object-none rounded"
+                            />
+                            <div className="w-full flex flex-col justify-around h-[70.7px]">
+                                <p className="whitespace-nowrap overflow-hidden text-ellipsis text-sm w-[218px] group-hover:text-primary">
+                                    {anime.title_english || anime.title}
+                                </p>
+                                <p>{anime.type}</p>
+                                <div className="flex gap-0.5 items-center">
+                                    <p>{anime.aired.string.split(" to")[0]}</p>
+                                    <DotFilledIcon className="opacity-30" />
+                                    <p>{anime.duration}</p>
+                                </div>
+                            </div>
+                        </Link>
+                    ))
+                )}
+            </div>
+        </div>
     );
 };
 
@@ -48,15 +64,19 @@ const SearchBox = () => {
     const [searchKey, setSearchKey] = useState<string>("");
     const [searchData, setSearchData] = useState<animeData[] | []>([]);
     const [showDropdown, setShowDropdown] = useState<boolean>(false);
+    const [showLoader, setShowLoader] = useState<boolean>(false);
     const previousController = useRef<AbortController | null>(null);
 
     useEffect(() => {
         if (searchKey.length < 2) {
+            setSearchData([]);
+            setShowLoader(false);
             setShowDropdown(false);
         }
 
         const getSearchResults = setTimeout(async () => {
             if (searchKey.length >= 2) {
+                setShowLoader(true);
                 if (previousController.current) {
                     previousController.current.abort();
                 }
@@ -71,6 +91,7 @@ const SearchBox = () => {
                 setSearchData(data);
                 setShowDropdown(true);
             }
+            setShowLoader(false);
         }, 500);
 
         return () => clearTimeout(getSearchResults);
@@ -99,7 +120,9 @@ const SearchBox = () => {
                     className="absolute right-3 top-1/2 -translate-y-1/2"
                 />
             </InputBox>
-            {showDropdown && <DisplaySearchAnimeResults data={searchData} />}
+            {(showDropdown || showLoader) && (
+                <DisplaySearchAnimeResults data={searchData} showLoader={showLoader} />
+            )}
         </div>
     );
 };
