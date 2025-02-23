cc8e75061be6a4fc5ce08615fc0b2e0bc912a602
rahulkanna <kannarahul05@gmail.com>
2025-02-23 14:51:16 +0530

    handled empty results

--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -9,7 +9,7 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
 interface SeachAnimePropTypes {
-    data: animeData[] | [];
+    data: animeData[] | [] | null;
     showLoader: boolean;
 }
 
@@ -23,53 +23,55 @@ const DisplaySearchAnimeResults = ({ data, showLoader }: SeachAnimePropTypes) =>
                     <span className="rounded-[100%] w-4 h-4 border bg-neutral-50 animate-loader-bounce delay-200"></span>
                 </div>
             )}
-            <div>
-                {isDataEmptyorUndefined(data) ? (
-                    <div className="p-3">No results found</div>
-                ) : (
-                    data.slice(0, 5).map((anime: animeData) => (
-                        <Link
-                            href=""
-                            key={anime.mal_id}
-                            className="w-full flex p-3 gap-2 border-b border-white/20 last:border-none hover:bg-white/10 group"
-                        >
-                            <Image
-                                src={anime.images.webp.large_image_url}
-                                alt={anime.title_english || anime.title}
-                                width={50}
-                                height={70}
-                                title={anime.title_english || anime.title}
-                                className="object-none rounded"
-                            />
-                            <div className="w-full flex flex-col justify-around h-[70.7px]">
-                                <p className="whitespace-nowrap overflow-hidden text-ellipsis text-sm w-[218px] group-hover:text-primary">
-                                    {anime.title_english || anime.title}
-                                </p>
-                                <p>{anime.type}</p>
-                                <div className="flex gap-0.5 items-center">
-                                    <p>{anime.aired.string.split(" to")[0]}</p>
-                                    <DotFilledIcon className="opacity-30" />
-                                    <p>{anime.duration}</p>
+            {data !== null && (
+                <div>
+                    {data.length === 0 ? (
+                        <div className="p-3">No results found</div>
+                    ) : (
+                        data.slice(0, 5).map((anime: animeData) => (
+                            <Link
+                                href=""
+                                key={anime.mal_id}
+                                className="w-full flex p-3 gap-2 border-b border-white/20 last:border-none hover:bg-white/10 group"
+                            >
+                                <Image
+                                    src={anime.images.webp.large_image_url}
+                                    alt={anime.title_english || anime.title}
+                                    width={50}
+                                    height={70}
+                                    title={anime.title_english || anime.title}
+                                    className="object-none rounded"
+                                />
+                                <div className="w-full flex flex-col justify-around h-[70.7px]">
+                                    <p className="whitespace-nowrap overflow-hidden text-ellipsis text-sm w-[218px] group-hover:text-primary">
+                                        {anime.title_english || anime.title}
+                                    </p>
+                                    <p>{anime.type}</p>
+                                    <div className="flex gap-0.5 items-center">
+                                        <p>{anime.aired.string.split(" to")[0]}</p>
+                                        <DotFilledIcon className="opacity-30" />
+                                        <p>{anime.duration}</p>
+                                    </div>
                                 </div>
-                            </div>
-                        </Link>
-                    ))
-                )}
-            </div>
+                            </Link>
+                        ))
+                    )}
+                </div>
+            )}
         </div>
     );
 };
 
 const SearchBox = () => {
     const [searchKey, setSearchKey] = useState<string>("");
-    const [searchData, setSearchData] = useState<animeData[] | []>([]);
+    const [searchData, setSearchData] = useState<animeData[] | [] | null>(null);
     const [showDropdown, setShowDropdown] = useState<boolean>(false);
     const [showLoader, setShowLoader] = useState<boolean>(false);
     const previousController = useRef<AbortController | null>(null);
 
     useEffect(() => {
         if (searchKey.length < 2) {
-            setSearchData([]);
+            setSearchData(null);
             setShowLoader(false);
             setShowDropdown(false);
         }
