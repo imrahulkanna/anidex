ab9095d611becd9d61dcfb7159d8330cf1fb92d8
rahulkanna <kannarahul05@gmail.com>
2025-02-28 21:14:21 +0530

    updated the margins

--- app/page.tsx
+++ app/page.tsx
@@ -3,22 +3,26 @@ import FeaturedAnimes from "@/components/FeaturedAnimes";
 import CategoryAnimes from "@/components/CategoryAnimes";
 import AnimeGenres from "@/components/AnimeGenres";
 import WeeklyReleases from "@/components/WeeklyReleases";
+import RecentReleases from "@/components/RecentReleases";
 
 export default function Home() {
     const featuredAnimeTitles = ["Top Airing", "Most Popular", "Most Favorite", "Latest Completed"];
     const categoryTitles = ["Latest Episodes", "New Releases", "Top Upcoming"];
     return (
-        <main className="mt-20 px-4 md:px-6 3xl:px-2">
-            <TrendingAnimeList />
+        <main>
+            <RecentReleases />
+            <div className="px-4 md:px-6 3xl:px-2">
+                <TrendingAnimeList />
+            </div>
             <div
                 id="featured-animes"
-                className="w-full mb-10 flex justify-between flex-wrap gap-x-4 gap-y-10 flex-col md:flex-row md:gap-x-0"
+                className="w-full mb-10 flex justify-between flex-wrap gap-x-4 gap-y-10 flex-col md:flex-row md:gap-x-0 px-4 md:px-6 3xl:px-2"
             >
                 {featuredAnimeTitles.map((title) => (
                     <FeaturedAnimes key={title} title={title} />
                 ))}
             </div>
-            <div className="w-full flex flex-col xl:flex-row">
+            <div className="w-full flex flex-col xl:flex-row px-4 md:px-6 3xl:px-2">
                 {/* Anime tiles */}
                 <div id="main-container" className="w-full xl:w-3/4">
                     {categoryTitles.map((title) => (
