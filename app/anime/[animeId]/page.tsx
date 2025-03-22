ef091c08694111453d7b595d799afb17825284b8
rahulkanna <kannarahul05@gmail.com>
2025-03-22 23:35:22 +0530

    made anime page responsive

--- app/anime/[animeId]/page.tsx
+++ app/anime/[animeId]/page.tsx
@@ -1,14 +1,15 @@
-import {getAnimeCharacters, getAnimeDataById, getAnimePromotionalVideos} from "@/app/lib/fetch";
-import {StopLoading, ViewMoreLessBtn} from "@/components/UtilityComponents";
-import {ClockIcon, DotFilledIcon} from "@radix-ui/react-icons";
+import { getAnimeCharacters, getAnimeDataById, getAnimePromotionalVideos } from "@/app/lib/fetch";
+import { StopLoading, ViewMoreLessBtn } from "@/components/UtilityComponents";
+import { ClockIcon, DotFilledIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
 import Link from "next/link";
-import {PlayCircleIcon as PlayCircleOutline} from "@heroicons/react/24/outline"
-import {PlayCircleIcon, PlayIcon } from "@heroicons/react/16/solid";
+import { PlayCircleIcon as PlayCircleOutline } from "@heroicons/react/24/outline";
+import { PlayCircleIcon, PlayIcon } from "@heroicons/react/16/solid";
 import AddToListButton from "@/components/AddToListButton";
-import {character, streamingPartner} from "@/types/ApiResponse";
+import { character, streamingPartner } from "@/types/ApiResponse";
 import dynamic from "next/dynamic";
-import {isDataEmptyorUndefined} from "@/app/lib/utils";
+import { isDataEmptyorUndefined } from "@/app/lib/utils";
+import AnimeGenres from "@/components/AnimeGenres";
 
 const Anime = async ({ params }: { params: { animeId: string } }) => {
     const { animeId } = params;
@@ -17,7 +18,7 @@ const Anime = async ({ params }: { params: { animeId: string } }) => {
     const promoVideos = await getAnimePromotionalVideos(parseInt(animeId));
 
     const PromotionVideoContainer = dynamic(
-        () => import('../../../components/PromotionVideoContainer'),
+        () => import("../../../components/PromotionVideoContainer"),
         { ssr: false }
     );
 
@@ -32,16 +33,16 @@ const Anime = async ({ params }: { params: { animeId: string } }) => {
                         style={{ backgroundImage: `url(${animeData.images.webp.large_image_url})` }}
                     ></div>
                 </div>
-                <div className="w-full flex">
+                <div className="w-full flex flex-col xl:flex-row">
                     {/* main column */}
-                    <div className="w-3/4 z-20 py-16">
-                        <div className="mx-auto my-auto flex gap-10 items-start justify-center w-10/12 2xl:w-8/12">
+                    <div className="w-full xl:w-3/4 z-20 py-16">
+                        <div className="mx-auto my-auto flex flex-col lg:flex-row gap-10 items-start justify-center w-10/12 2xl:w-8/12">
                             <Image
                                 src={animeData.images.webp.large_image_url}
                                 alt={animeData.title_english || animeData.title}
                                 height={0}
                                 width={140}
-                                className="w-52"
+                                className="w-52 mx-auto"
                             />
                             <div className="flex flex-col gap-5 text-sm">
                                 <h1 className="text-5xl font-bold">
@@ -67,27 +68,33 @@ const Anime = async ({ params }: { params: { animeId: string } }) => {
                                         <p>{animeData.duration?.split(" per ep")}</p>
                                     </div>
                                 </div>
-                                <div className="flex gap-3 justify-start items-center">
-                                    <button
-                                        className="h-9 px-5 py-2 shadow rounded-full font-semibold flex gap-1 items-center justify-center bg-primary text-neutral-900 text-base hover:bg-primary/80"
-                                    >
+                                <div className="flex flex-wrap gap-3 justify-start items-center">
+                                    <button className="h-9 px-5 py-2 shadow rounded-full font-semibold flex gap-1 items-center justify-center bg-primary text-neutral-900 text-base hover:bg-primary/80">
                                         <PlayIcon className="w-5 h-5" />
                                         Watch
                                     </button>
-                                    <AddToListButton anime={animeData} dropDownPositionStyle="top" btnStyle="px-5 text-base" btnWidth="w-44" />
-                                </div>
-                                <div>
-                                    <p className="line-clamp-3 leading-relaxed text-sm" id="synopsis-container">
-                                        {animeData.synopsis}
-                                    </p>
-                                    <ViewMoreLessBtn
-                                        styles="text-xs font-semibold"
+                                    <AddToListButton
+                                        anime={animeData}
+                                        dropDownPositionStyle="top"
+                                        btnStyle="px-5 text-base"
+                                        btnWidth="w-44"
                                     />
                                 </div>
-                                { animeData.streaming.length > 0 && (
+                                <div>
+                                    <p
+                                        className="line-clamp-3 leading-relaxed text-sm"
+                                        id="synopsis-container"
+                                    >
+                                        {animeData.synopsis}
+                                    </p>
+                                    <ViewMoreLessBtn styles="text-xs font-semibold" />
+                                </div>
+                                {animeData.streaming.length > 0 && (
                                     <div>
-                                        <div className="font-semibold text-lg text-primary">Streaming on</div>
-                                        <div className="grid grid-cols-2 gap-x-16 gap-y-3 px-4 font-medium">
+                                        <div className="font-semibold text-lg text-primary">
+                                            Streaming on
+                                        </div>
+                                        <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-16 gap-y-3 px-4 font-medium">
                                             {animeData.streaming.map((stream: streamingPartner) => (
                                                 <Link
                                                     href={stream.url}
@@ -110,7 +117,7 @@ const Anime = async ({ params }: { params: { animeId: string } }) => {
                         </div>
                     </div>
                     {/* side column */}
-                    <div className="w-[calc(25%-30px)] flex flex-col justify-center gap-3 text-sm px-7 py-16 bg-white/5 backdrop-blur-xl">
+                    <div className="w-full xl:w-[calc(25%-30px)] flex flex-col justify-center gap-3 text-sm px-7 py-16 bg-white/5 backdrop-blur-xl">
                         <div>
                             <span className="font-bold">Japanese:</span>{" "}
                             <span>{animeData.title_japanese}</span>
@@ -142,7 +149,10 @@ const Anime = async ({ params }: { params: { animeId: string } }) => {
                         <div className="flex items-center gap-2 flex-wrap border-y-[0.5px] border-y-white/30 py-3">
                             <span className="font-bold">Genres:</span>
                             {animeData.genres.map((genre: { name: string }) => (
-                                <div key={genre.name} className="py-1 px-2 border border-white/30 rounded-3xl text-xs">
+                                <div
+                                    key={genre.name}
+                                    className="py-1 px-2 border border-white/30 rounded-3xl text-xs"
+                                >
                                     {genre.name}
                                 </div>
                             ))}
@@ -166,44 +176,62 @@ const Anime = async ({ params }: { params: { animeId: string } }) => {
                     </div>
                 </div>
             </div>
-            <div className='w-full px-4 md:px-6 3xl:px-2'>
-                {/* characters section */}
-                {characters && (
-                    <div id="characters" className="mt-10 md:px-4">
-                        <h2 className="text-2xl font-bold text-primary mb-5">Characters & Voice Actors</h2>
-                        <div className="flex flex-wrap gap-4 mx-auto">
-                            {characters.splice(0,9).map((character: character) => (
-                                <div key={character.character.mal_id} className="flex flex-grow items-center p-3 bg-white/10 rounded-md">
-                                    <Image
-                                        src={character.character.images.webp.image_url}
-                                        alt={character.character.name}
-                                        height={0}
-                                        width={200}
-                                        className="w-11 h-11 rounded-full object-cover"
-                                    />
-                                    <div className="w-52 px-2 py-1 flex flex-col gap-1 justify-between text-sm">
-                                        <p>
-                                            <span className="font-bold">{character.character.name}</span>{", "}
-                                            <span className="text-xs">{character.role}</span>
-                                        </p>
-                                        <p className="text-right">{character.voice_actors[0].person.name}</p>
+            <div className="w-full flex flex-col xl:flex-row mt-10 px-4 md:px-6 3xl:px-2">
+                <div className="w-full xl:w-3/4">
+                    {/* characters section */}
+                    {characters && (
+                        <div id="characters" className="md:px-4">
+                            <h2 className="text-2xl font-bold text-primary mb-5">
+                                Characters & Voice Actors
+                            </h2>
+                            <div className="flex flex-wrap gap-4 mx-auto">
+                                {characters.splice(0, 9).map((character: character) => (
+                                    <div
+                                        key={character.character.mal_id}
+                                        className="flex flex-grow items-center p-3 bg-white/10 rounded-md"
+                                    >
+                                        <Image
+                                            src={character.character.images.webp.image_url}
+                                            alt={character.character.name}
+                                            height={0}
+                                            width={200}
+                                            className="w-11 h-11 rounded-full object-cover"
+                                        />
+                                        <div className="w-52 px-2 py-1 flex flex-col gap-1 justify-between text-sm">
+                                            <p>
+                                                <span className="font-bold">
+                                                    {character.character.name}
+                                                </span>
+                                                {", "}
+                                                <span className="text-xs">{character.role}</span>
+                                            </p>
+                                            <p className="text-right">
+                                                {character.voice_actors[0].person.name}
+                                            </p>
+                                        </div>
+                                        <Image
+                                            src={
+                                                character.voice_actors[0].person.images.jpg
+                                                    .image_url
+                                            }
+                                            alt={character.voice_actors[0].person.name}
+                                            height={0}
+                                            width={200}
+                                            className="w-11 h-11 rounded-full object-cover grayscale"
+                                        />
                                     </div>
-                                    <Image
-                                        src={character.voice_actors[0].person.images.jpg.image_url}
-                                        alt={character.voice_actors[0].person.name}
-                                        height={0}
-                                        width={200}
-                                        className="w-11 h-11 rounded-full object-cover grayscale"
-                                    />
-                                </div>
-                            ))}
+                                ))}
+                            </div>
                         </div>
-                    </div>
-                )}
-                {/* promotional videos section */}
-                {!isDataEmptyorUndefined(promoVideos) && (
-                    <PromotionVideoContainer promoVideos={promoVideos.splice(0,4)} />
-                )}
+                    )}
+                    {/* promotional videos section */}
+                    {!isDataEmptyorUndefined(promoVideos) && (
+                        <PromotionVideoContainer promoVideos={promoVideos.splice(0, 4)} />
+                    )}
+                </div>
+                <div className="w-full xl:w-1/4">
+                    <AnimeGenres />
+                </div>
             </div>
         </div>
     );
